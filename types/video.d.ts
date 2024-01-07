4acebb1867776053fed75942fd167314c78610af
Afrokk <afrasiyabkhan379@gmail.com>
2024-01-07 06:53:13 -0500

    fix: update video type

--- types/video.d.ts
+++ types/video.d.ts
@@ -4,10 +4,38 @@ export type Video = {
   };
   snippet: {
     title: string;
+    channelId: string;
+    channelTitle: string;
+    description: string;
+    playlistId: string;
+    position: number;
+    publishedAt: string;
+    resourceId: {
+      kind: string;
+      videoId: string;
+    };
     thumbnails: {
+      default: {
+        url: string;
+      };
       medium: {
         url: string;
       };
+      high: {
+        url: string;
+      };
+      standard: {
+        url: string;
+      };
     };
+    videoOwnerChannelId: string;
+    videoOwnerChannelTitle: string;
   };
+  etag: string;
+  kind: string;
 };
+
+export interface Cache {
+  videos: Video[] | null;
+  fetchTime: number | null;
+}
