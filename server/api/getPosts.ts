8301fefbae6b2df84ca05c8b406cf3dc0618d016
Afrokk <afrasiyabkhan379@gmail.com>
2024-01-20 04:55:00 -0500

    chore: run linter

--- server/api/getPosts.ts
+++ server/api/getPosts.ts
@@ -6,15 +6,14 @@ export default defineEventHandler(async (event) => {
       frame:
         '<iframe width="100%" src="https://www.instagram.com/p/CxvBaGCIApU/embed" frameborder="0" scrolling="no" allowtransparency="true"></iframe>',
       caption: "iPhone 15 Pro is OVERHEATING🔥 Here's WHY Its happening. Have you experienced it?",
-      likeCount: "60,440",
-    
+      likeCount: '60,440',
     },
     {
       frame:
         '<iframe width="100%" src="https://www.instagram.com/p/CgHdZQAIdtQ/embed" frameborder="0" scrolling="no" allowtransparency="true"></iframe>',
       caption:
         'Great time catching up with the CEO himself @getpeid at @nothing store launch in London. The Team is definitely bringing that "fire" back to tech!🔥',
-      likeCount: "32,712",
+      likeCount: '32,712',
     },
   ];
 
