cdfa78dc03a412494e11452a18bc54bec1f8390c
Afrokk <afrasiyabkhan379@gmail.com>
2024-01-24 07:03:44 -0500

    WIP: add hero section on work page
    This change adds the hero section on the work page.
    - Implements Rocket.vue
    - Integrates UI components
    - Still WIP

--- pages/work.vue
+++ pages/work.vue
@@ -1,157 +1,161 @@
 <template>
-  <div class="bg-grey flex flex-col items-start justify-center pt-20">
-    <section
-      id="statisticsSection"
-      ref="statisticsSection"
-      class="z-20 pb-16 max-w-screen-xl mx-auto flex flex-wrap justify-center items-center"
-    >
+  <section
+    class="relative z-20 flex flex-col justify-center items-center text-center mx-auto text-white min-h-screen max-w-screen-xl px-4"
+  >
+    <div class="w-full pt-16 sm:w-2/4">
+      <Rocket />
+    </div>
+    <div class="space-y-2" id="animated-text">
+      <h2
+        class="text-3xl xsm:text-6xl mt-16 pb-4 text-zinc-200 text-center font-bold tracking-tighter sm:text-6xl xl:text-7xl/none"
+      >
+        <span class="gradient-text">Work</span> With Us.
+      </h2>
+      <p
+        class="text-zinc-100 flex flex-col pb-16 text-sm xsm:text-base tracking-normal md:text-base md:mx-20"
+        v-html="data ? data.mainDescription : ''"
+      ></p>
+    </div>
+    <DownArrow
+      :class="'text-orange-500'"
+      :sizeClass="'h-8 w-8'"
+      :postionClass="'bottom-3 left-1/2'"
+      @click="($refs.statsSection as HTMLElement)?.scrollIntoView({ behavior: 'smooth' })"
+    />
+  </section>
+  <section class="bg-grey flex flex-col items-start justify-center">
+    <div class="z-20 max-w-screen-xl mx-auto flex flex-wrap justify-center items-center">
       <div class="container px-4">
-        <div class="space-y-2 flex flex-col" id="animated-text">
-          <h2
-            class="text-3xl xsm:text-6xl mt-16 pb-4 text-zinc-200 text-center font-bold tracking-tighter sm:text-6xl xl:text-7xl/none"
-          >
-            <span class="gradient-text">Work</span> With Us.
-          </h2>
-          <p
-            class="text-zinc-100 flex flex-col text-sm xsm:text-base md:text-base pb-8 md:mx-20"
-            v-html="content ? content.mainDescription : ''"
-          ></p>
-        </div>
-        <div class="grid gap-6 items-center">
-          <div class="flex flex-col justify-center space-y-8 text-center">
-            <div class="space-y-2" id="animated-text">
-              <h2
-                class="text-3xl xsm:text-6xl pb-0 pt-5 text-zinc-200 text-center font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+        <div class="flex flex-col justify-center text-center" ref="statsSection">
+          <Separator />
+          <div id="animated-text">
+            <h2
+              class="text-3xl xsm:text-6xl mt-16 pb-4 text-zinc-200 text-center font-bold tracking-tighter sm:text-6xl xl:text-7xl/none"
+            >
+              The <span class="gradient-text" id="statAnimationAnchor">Numbers</span>
+            </h2>
+          </div>
+          <div class="w-full max-w-full mx-auto">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-center">
+              <div
+                class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
               >
-                The <span class="gradient-text">Numbers</span>
-              </h2>
-            </div>
-            <div class="w-full max-w-full mx-auto">
-              <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-center">
-                <div
-                  class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
-                >
-                  <div className="p-2 rounded-full">
-                    <font-awesome-icon
-                      :icon="faYoutube"
-                      class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-red-600"
-                    />
-                    <h5 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">YouTube</h5>
-                  </div>
-                  <div
-                    class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
-                  >
-                    <CountUp :endVal="statCurrentVals[0]" />
-                    <span class="ml-1 text-zinc-200">+</span>
-                  </div>
-                  <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Total Subscribers</p>
+                <div className="p-2 rounded-full">
+                  <font-awesome-icon
+                    :icon="faYoutube"
+                    class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-red-600"
+                  />
+                  <h5 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">YouTube</h5>
                 </div>
                 <div
-                  class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+                  class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
                 >
-                  <div className="p-2 rounded-full">
-                    <font-awesome-icon
-                      :icon="faInstagram"
-                      class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-purple-600"
-                    />
-                    <h5 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">
-                      Instagram
-                    </h5>
-                  </div>
-                  <div
-                    class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
-                  >
-                    <CountUp :endVal="statCurrentVals[1]" />
-                    <span class="ml-1 text-zinc-200">+</span>
-                  </div>
-                  <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Total Followers</p>
+                  <CountUp :endVal="statCurrentVals[0]" />
+                  <span class="ml-1 text-zinc-200">+</span>
+                </div>
+                <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Total Subscribers</p>
+              </div>
+              <div
+                class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+              >
+                <div className="p-2 rounded-full">
+                  <font-awesome-icon
+                    :icon="faInstagram"
+                    class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-purple-600"
+                  />
+                  <h5 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">Instagram</h5>
                 </div>
                 <div
-                  class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+                  class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
                 >
-                  <div className="p-2 rounded-full">
-                    <font-awesome-icon
-                      :icon="faTwitter"
-                      class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-blue-500"
-                    />
-                    <h5 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">Twitter</h5>
-                  </div>
-                  <div
-                    class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
-                  >
-                    <CountUp :endVal="statCurrentVals[2]" />
-                    <span class="ml-1 text-zinc-200">+</span>
-                  </div>
-                  <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Total Followers</p>
+                  <CountUp :endVal="statCurrentVals[1]" />
+                  <span class="ml-1 text-zinc-200">+</span>
+                </div>
+                <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Total Followers</p>
+              </div>
+              <div
+                class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+              >
+                <div className="p-2 rounded-full">
+                  <font-awesome-icon
+                    :icon="faTwitter"
+                    class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-blue-500"
+                  />
+                  <h5 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">Twitter</h5>
                 </div>
                 <div
-                  class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+                  class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
                 >
-                  <div className="p-2 rounded-full">
-                    <font-awesome-icon
-                      :icon="faYoutube"
-                      class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-red-600"
-                    />
-                    <h5 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">YouTube</h5>
-                  </div>
-                  <div
-                    class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
-                  >
-                    <CountUp :endVal="statCurrentVals[3]" />
-                    <span class="ml-2">Million</span>
-                    <span class="ml-1 text-zinc-200">+</span>
-                  </div>
-                  <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Total Views</p>
+                  <CountUp :endVal="statCurrentVals[2]" />
+                  <span class="ml-1 text-zinc-200">+</span>
+                </div>
+                <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Total Followers</p>
+              </div>
+              <div
+                class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+              >
+                <div className="p-2 rounded-full">
+                  <font-awesome-icon
+                    :icon="faYoutube"
+                    class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-red-600"
+                  />
+                  <h5 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">YouTube</h5>
                 </div>
                 <div
-                  class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+                  class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
                 >
-                  <div className="p-2 rounded-full">
-                    <font-awesome-icon
-                      :icon="faInstagram"
-                      class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-purple-600"
-                    />
-                    <h5 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">
-                      Instagram
-                    </h5>
-                  </div>
-                  <div
-                    class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
-                  >
-                    <CountUp :endVal="statCurrentVals[4]" />
-                    <span class="ml-1 text-zinc-200">+</span>
-                  </div>
-                  <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Video Views</p>
+                  <CountUp :endVal="statCurrentVals[3]" />
+                  <span class="ml-2">Million</span>
+                  <span class="ml-1 text-zinc-200">+</span>
+                </div>
+                <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Total Views</p>
+              </div>
+              <div
+                class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+              >
+                <div className="p-2 rounded-full">
+                  <font-awesome-icon
+                    :icon="faInstagram"
+                    class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-purple-600"
+                  />
+                  <h5 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">Instagram</h5>
                 </div>
                 <div
-                  class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+                  class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
                 >
-                  <div className="p-2 rounded-full">
-                    <font-awesome-icon
-                      :icon="faTiktok"
-                      class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-pink-600"
-                    />
-                    <h2 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">TikTok</h2>
-                  </div>
-                  <div
-                    class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
-                  >
-                    <CountUp :endVal="statCurrentVals[5]" />
-                    <span class="ml-1 text-zinc-200">+</span>
-                  </div>
-                  <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Total Likes</p>
+                  <CountUp :endVal="statCurrentVals[4]" />
+                  <span class="ml-1 text-zinc-200">+</span>
                 </div>
+                <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Video Views</p>
+              </div>
+              <div
+                class="grid-item flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+              >
+                <div className="p-2 rounded-full">
+                  <font-awesome-icon
+                    :icon="faTiktok"
+                    class="w-28 h-28 xsm:h-24 xsm:w-24 md:w-16 md:h-16 text-pink-600"
+                  />
+                  <h2 class="font-bold text-zinc-200 text-lg xsm:text-xl md:text-lg">TikTok</h2>
+                </div>
+                <div
+                  class="flex items-center text-2xl xsm:text-6xl text-center font-bold tracking-tighter xl:text-6xl/none text-orange-500"
+                >
+                  <CountUp :endVal="statCurrentVals[5]" />
+                  <span class="ml-1 text-zinc-200">+</span>
+                </div>
+                <p class="text-zinc-200 pt-2 xsm:text-xl mg:text-lg">Total Likes</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </section>
-    <div id="brandsSection" class="carousel-container bg-gray pt-16 overflow-x-hidden">
+    </div>
+    <section id="brandsSection" class="carousel-container bg-gray pt-20 overflow-x-hidden">
       <h2
         class="text-3xl pb-8 text-zinc-200 text-center font-bold tracking-tighter sm:text-4xl xl:text-5xl"
       >
-        Brands we've collaborted with
+        Brands we've worked with
       </h2>
       <div class="carousel pt-4 flex">
         <div
@@ -171,8 +175,20 @@
           />
         </div>
       </div>
-    </div>
-  </div>
+    </section>
+    <Separator />
+    <section
+      id="brandsSection"
+      class="carousel-container flex flex-col text-center justify-center items-center bg-gray pt-0 overflow-x-hidden"
+    >
+      <h2
+        class="text-3xl pb-8 text-zinc-200 text-center font-bold tracking-tighter sm:text-4xl xl:text-5xl"
+      >
+        Previous Collaborations
+      </h2>
+      <p class="text-white h-12 w-12">WIP</p>
+    </section>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -187,11 +203,18 @@ gsap.registerPlugin(ScrollTrigger);
 let isCountStarted = false;
 let statCurrentVals = ref(Array(6).fill(0));
 
-const { data: content } = await useAsyncData('work', () => queryContent('/work').findOne());
+let data: Ref;
 
-defineExpose({
-  data: content,
-});
+try {
+  const result = await useAsyncData('work', () => queryContent('/work').findOne());
+  data = ref(result.data.value);
+  if (data.value.mainDescription == null) {
+    data.value.mainDescription = 'Error fetching data.';
+  }
+} catch (error) {
+  console.error('Failed to fetch data:', error);
+  data = ref('Failed to fetch data.');
+}
 
 const statEndVals = [1710000, 115000, 72000, 750, 65000, 630000];
 const logos = [
@@ -245,7 +268,7 @@ onMounted(() => {
 
   gsap.from('.grid-item', {
     scrollTrigger: {
-      trigger: '#statisticsSection',
+      trigger: '#statAnimationAnchor',
       start: 'top center',
       onEnter: () => {
         if (!isCountStarted) {
