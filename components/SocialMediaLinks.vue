5b53fbb529785d7f6e251b7c6150028743001d08
Afrokk <afrasiyabkhan379@gmail.com>
2024-01-23 23:26:53 -0500

    feat: add props to SocialMediaLinks component

--- components/SocialMediaLinks.vue
+++ components/SocialMediaLinks.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex space-x-2 sm:space-x-4 justify-center">
     <a
-      href="https://www.youtube.com/user/XEETECHCARE"
+      v-if="youtubeLink.show"
+      :href="youtubeLink.link"
       target="_blank"
       rel="noopener noreferrer"
       class="bg-zinc-200 text-gray hover:text-red-600 rounded-full flex items-center justify-center h-8 w-8 sm:h-10 sm:w-10"
@@ -9,7 +10,8 @@
       <font-awesome-icon :icon="youtubeIcon" class="h-5 w-5 sm:h-6 sm:w-6" />
     </a>
     <a
-      href="https://www.instagram.com/xeetechcare/"
+      v-if="instagramLink.show"
+      :href="instagramLink.link"
       target="_blank"
       rel="noopener noreferrer"
       class="bg-zinc-300 text-gray hover:text-purple-600 rounded-full flex items-center justify-center h-8 w-8 sm:h-10 sm:w-10"
@@ -17,7 +19,8 @@
       <font-awesome-icon :icon="instagramIcon" class="h-5 w-5 sm:h-6 sm:w-6" />
     </a>
     <a
-      href="https://twitter.com/XEETECHCARE"
+      v-if="twitterLink.show"
+      :href="twitterLink.link"
       target="_blank"
       rel="noopener noreferrer"
       class="bg-zinc-300 text-gray hover:text-blue-600 rounded-full flex items-center justify-center h-8 w-8 sm:h-10 sm:w-10"
@@ -25,7 +28,8 @@
       <font-awesome-icon :icon="twitterIcon" class="h-5 w-5 sm:h-6 sm:w-6" />
     </a>
     <a
-      href="https://www.tiktok.com/@xeetechcare0"
+      v-if="tiktokLink.show"
+      :href="tiktokLink.link"
       target="_blank"
       rel="noopener noreferrer"
       class="bg-zinc-300 text-gray hover:text-pink-600 rounded-full flex items-center justify-center h-8 w-8 sm:h-10 sm:w-10"
@@ -33,7 +37,8 @@
       <font-awesome-icon :icon="tiktokIcon" class="h-5 w-5 sm:h-6 sm:w-6" />
     </a>
     <a
-      href="mailto:xeetechcare@delkatalents.com"
+      v-if="email.show"
+      :href="'mailto:' + email.link"
       target="_blank"
       rel="noopener noreferrer"
       class="bg-zinc-300 text-gray hover:text-orange-500 rounded-full flex items-center justify-center h-8 w-8 sm:h-10 sm:w-10"
@@ -57,6 +62,44 @@ const envelopeIcon = faEnvelope;
 defineComponent({
   components: {
     FontAwesomeIcon,
+  },
+});
+
+defineProps({
+  twitterLink: {
+    type: Object,
+    default: () => ({
+      link: 'https://twitter.com/xeetechcare',
+      show: true,
+    }),
+  },
+  instagramLink: {
+    type: Object,
+    default: () => ({
+      link: 'https://www.instagram.com/xeetechcare',
+      show: true,
+    }),
+  },
+  youtubeLink: {
+    type: Object,
+    default: () => ({
+      link: 'https://www.youtube.com/xeetechcare',
+      show: true,
+    }),
+  },
+  tiktokLink: {
+    type: Object,
+    default: () => ({
+      link: 'https://www.tiktok.com/@xeetechcare0',
+      show: true,
+    }),
+  },
+  email: {
+    type: Object,
+    default: () => ({
+      link: 'xeetechcare@delkatalents.com',
+      show: true,
+    }),
   },
 });
 </script>
