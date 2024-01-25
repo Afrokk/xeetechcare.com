e4351075716e5ae6c8227845c71dfe37f85e1c0e
Afrokk <afrasiyabkhan379@gmail.com>
2024-01-25 11:02:24 -0500

    chore: accessibility optimizations

---
+++ components/ui/DownArrow.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    :class="[
+      positionClass,
+      'transform -translate-x-1/2 animate-bounce cursor-pointer transition-all hover:scale-110 z-30',
+    ]"
+  >
+    <font-awesome-icon :class="sizeClass" :icon="faArrowDown" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+
+defineProps({
+  sizeClass: {
+    type: String,
+    default: 'h-6 w-6 xsm:h-7 xsm:w-7',
+  },
+  positionClass: {
+    type: String,
+    default: 'inline',
+  },
+});
+
+defineExpose({ FontAwesomeIcon, faArrowDown });
+</script>
+
+<style scoped>
+@keyframes bounce {
+  0%,
+  100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+}
+
+.animate-bounce {
+  animation: bounce 1.6s infinite;
+}
+</style>
