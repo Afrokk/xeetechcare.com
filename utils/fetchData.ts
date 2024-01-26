8311ec18ae3dd4695796ddc4182a12fe09b5a296
Afrokk <afrasiyabkhan379@gmail.com>
2024-01-26 06:49:21 -0500

    refactor: fix verbose code
    Other changes include:
    - run linter

--- utils/fetchData.ts
+++ utils/fetchData.ts
@@ -3,16 +3,12 @@
  * @param endpoint The endpoint to fetch data from.
  * @returns A Promise that resolves to the fetched data.
  */
-export default async function fetchData(endpoint: string) {
-  let data: Ref;
-
+export default async function fetchData(endpoint: string): Promise<Ref<any>> {
   try {
-    const result = await useAsyncData('endpoint', () => queryContent(`/${endpoint}`).findOne());
-    data = ref(result.data.value);
-    return data;
+    const result = await useAsyncData(endpoint, () => queryContent(`/${endpoint}`).findOne());
+    return ref(result.data.value);
   } catch (error) {
     console.error('Failed to fetch data:', error);
-    data = ref('Failed to fetch data.');
-    return data;
+    return ref('Failed to fetch data.');
   }
 }
